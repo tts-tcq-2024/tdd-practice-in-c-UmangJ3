9767fccba83541c3e035dce398a39b5a89f9001a
UmangJ3 <haharofl14@gmail.com>
2024-07-20 16:33:13 +0530

    Update in StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdexcept>
 
 int is_empty(const char* numbers) {
     return numbers == NULL || strlen(numbers) == 0;
@@ -14,14 +15,64 @@ int convert_and_sum(const char* token) {
     return 0;
 }
 
+void check_for_negatives(const char* numbers, const char* delimiter) {
+    char* numbersCopy = strdup(numbers);
+    char* token = strtok(numbersCopy, delimiter);
+    char negative_numbers[256] = "";
+    int found_negative = 0;
+
+    while (token != NULL) {
+        int num = atoi(token);
+        if (num < 0) {
+            if (found_negative) {
+                strcat(negative_numbers, ",");
+            }
+            strcat(negative_numbers, token);
+            found_negative = 1;
+        }
+        token = strtok(NULL, delimiter);
+    }
+
+    free(numbersCopy);
+
+    if (found_negative) {
+        char error_message[300];
+        sprintf(error_message, "negatives not allowed: %s", negative_numbers);
+        throw std::runtime_error(error_message);
+    }
+}
+
+char* get_delimiter(const char* numbers) {
+    if (numbers[0] == '/' && numbers[1] == '/') {
+        const char* delimiter_start = numbers + 2;
+        const char* delimiter_end = strchr(delimiter_start, '\n');
+        if (delimiter_end) {
+            size_t len = delimiter_end - delimiter_start;
+            if (delimiter_start[0] == '[' && delimiter_start[len-1] == ']') {
+                delimiter_start++;
+                len -= 2;
+            }
+            char* delimiter = (char*)malloc(len + 1);
+            strncpy(delimiter, delimiter_start, len);
+            delimiter[len] = '\0';
+            return delimiter;
+        }
+    }
+    return strdup(",\n");
+}
+
 int add(const char* numbers) {
     if (is_empty(numbers)) {
         return 0;
     }
 
+    char* delimiter = get_delimiter(numbers);
+    const char* num_start = (numbers[0] == '/' && numbers[1] == '/') ? strchr(numbers, '\n') + 1 : numbers;
+    
+    check_for_negatives(num_start, delimiter);
+
     int sum = 0;
-    const char* delimiter = ",\n";
-    char* numbersCopy = strdup(numbers);
+    char* numbersCopy = strdup(num_start);
     char* token = strtok(numbersCopy, delimiter);
 
     while (token != NULL) {
@@ -30,5 +81,6 @@ int add(const char* numbers) {
     }
 
     free(numbersCopy);
+    free(delimiter);
     return sum;
 }
