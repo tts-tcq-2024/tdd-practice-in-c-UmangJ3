0dc95666c39ad86d73e56d72ba96f157f616698a
UmangJ3 <haharofl14@gmail.com>
2024-07-20 16:39:03 +0530

    Update in StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -15,6 +15,17 @@ int convert_and_sum(const char* token) {
     return 0;
 }
 
+void accumulate_negatives(const char* token, char* negative_numbers, int* found_negative) {
+    int num = atoi(token);
+    if (num < 0) {
+        if (*found_negative) {
+            strcat(negative_numbers, ",");
+        }
+        strcat(negative_numbers, token);
+        *found_negative = 1;
+    }
+}
+
 void check_for_negatives(const char* numbers, const char* delimiter) {
     char* numbersCopy = strdup(numbers);
     char* token = strtok(numbersCopy, delimiter);
@@ -22,14 +33,7 @@ void check_for_negatives(const char* numbers, const char* delimiter) {
     int found_negative = 0;
 
     while (token != NULL) {
-        int num = atoi(token);
-        if (num < 0) {
-            if (found_negative) {
-                strcat(negative_numbers, ",");
-            }
-            strcat(negative_numbers, token);
-            found_negative = 1;
-        }
+        accumulate_negatives(token, negative_numbers, &found_negative);
         token = strtok(NULL, delimiter);
     }
 
@@ -42,23 +46,45 @@ void check_for_negatives(const char* numbers, const char* delimiter) {
     }
 }
 
+char* extract_delimiter(const char* numbers) {
+    const char* delimiter_start = numbers + 2;
+    const char* delimiter_end = strchr(delimiter_start, '\n');
+    if (delimiter_end) {
+        size_t len = delimiter_end - delimiter_start;
+        if (delimiter_start[0] == '[' && delimiter_start[len-1] == ']') {
+            delimiter_start++;
+            len -= 2;
+        }
+        char* delimiter = (char*)malloc(len + 1);
+        strncpy(delimiter, delimiter_start, len);
+        delimiter[len] = '\0';
+        return delimiter;
+    }
+    return NULL;
+}
+
 char* get_delimiter(const char* numbers) {
     if (numbers[0] == '/' && numbers[1] == '/') {
-        const char* delimiter_start = numbers + 2;
-        const char* delimiter_end = strchr(delimiter_start, '\n');
-        if (delimiter_end) {
-            size_t len = delimiter_end - delimiter_start;
-            if (delimiter_start[0] == '[' && delimiter_start[len-1] == ']') {
-                delimiter_start++;
-                len -= 2;
-            }
-            char* delimiter = (char*)malloc(len + 1);
-            strncpy(delimiter, delimiter_start, len);
-            delimiter[len] = '\0';
+        char* delimiter = extract_delimiter(numbers);
+        if (delimiter) {
             return delimiter;
         }
     }
     return strdup(",\n");
+}
+
+int sum_tokens(const char* numbers, const char* delimiter) {
+    int sum = 0;
+    char* numbersCopy = strdup(numbers);
+    char* token = strtok(numbersCopy, delimiter);
+
+    while (token != NULL) {
+        sum += convert_and_sum(token);
+        token = strtok(NULL, delimiter);
+    }
+
+    free(numbersCopy);
+    return sum;
 }
 
 int add(const char* numbers) {
@@ -68,19 +94,10 @@ int add(const char* numbers) {
 
     char* delimiter = get_delimiter(numbers);
     const char* num_start = (numbers[0] == '/' && numbers[1] == '/') ? strchr(numbers, '\n') + 1 : numbers;
-    
+
     check_for_negatives(num_start, delimiter);
+    int sum = sum_tokens(num_start, delimiter);
 
-    int sum = 0;
-    char* numbersCopy = strdup(num_start);
-    char* token = strtok(numbersCopy, delimiter);
-
-    while (token != NULL) {
-        sum += convert_and_sum(token);
-        token = strtok(NULL, delimiter);
-    }
-
-    free(numbersCopy);
     free(delimiter);
     return sum;
 }
