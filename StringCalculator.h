d96df99dec27306c5a172da6844d85f4928e540e
UmangJ3 <haharofl14@gmail.com>
2024-07-20 16:52:00 +0530

    Update in StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -9,10 +9,7 @@ int is_empty(const char* numbers) {
 
 int convert_and_sum(const char* token) {
     int num = atoi(token);
-    if (num <= 1000) {
-        return num;
-    }
-    return 0;
+    return (num <= 1000) ? num : 0;
 }
 
 void accumulate_negatives(const char* token, char* negative_numbers, int* found_negative) {
@@ -53,7 +50,7 @@ char* create_delimiter(const char* delimiter_start, size_t len) {
     return delimiter;
 }
 
-char* extract_custom_delimiter(const char* numbers) {
+char* extract_delimiter(const char* numbers) {
     const char* delimiter_start = numbers + 2;
     const char* delimiter_end = strchr(delimiter_start, '\n');
     if (delimiter_end) {
@@ -67,28 +64,27 @@ char* extract_custom_delimiter(const char* numbers) {
     return NULL;
 }
 
+char* default_delimiter() {
+    return strdup(",\n");
+}
+
 char* get_delimiter(const char* numbers) {
     if (numbers[0] == '/' && numbers[1] == '/') {
-        char* delimiter = extract_custom_delimiter(numbers);
+        char* delimiter = extract_delimiter(numbers);
         if (delimiter) {
             return delimiter;
         }
     }
-    return strdup(",\n");
+    return default_delimiter();
 }
 
-char* prepare_numbers_copy(const char* numbers, const char* delimiter) {
-    char* numbersCopy = strdup(numbers);
-    if (numbers[0] == '/' && numbers[1] == '/') {
-        const char* num_start = strchr(numbers, '\n') + 1;
-        return strdup(num_start);
-    }
-    return numbersCopy;
+char* extract_numbers_part(const char* numbers) {
+    return (numbers[0] == '/' && numbers[1] == '/') ? strchr(numbers, '\n') + 1 : (char*)numbers;
 }
 
 int sum_tokens(const char* numbers, const char* delimiter) {
     int sum = 0;
-    char* numbersCopy = prepare_numbers_copy(numbers, delimiter);
+    char* numbersCopy = strdup(numbers);
     char* token = strtok(numbersCopy, delimiter);
 
     while (token != NULL) {
@@ -106,7 +102,7 @@ int add(const char* numbers) {
     }
 
     char* delimiter = get_delimiter(numbers);
-    const char* num_start = (numbers[0] == '/' && numbers[1] == '/') ? strchr(numbers, '\n') + 1 : numbers;
+    const char* num_start = extract_numbers_part(numbers);
     
     check_for_negatives(num_start, delimiter);
     int sum = sum_tokens(num_start, delimiter);
