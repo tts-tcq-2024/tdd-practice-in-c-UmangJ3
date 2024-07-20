1b983692e4059feae1bd27bd74662df1c0288ca4
UmangJ3 <haharofl14@gmail.com>
2024-07-20 16:08:35 +0530

    Update in StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,1 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int add(const char* numbers) {
+    if (numbers == NULL || strlen(numbers) == 0) {
+        return 0;
+    }
+
+    int sum = 0;
+    const char* delimiter = ",\n";
+    char* numbersCopy = strdup(numbers);
+    char* token = strtok(numbersCopy, delimiter);
+
+    while (token != NULL) {
+        int num = atoi(token);
+        if (num <= 1000) {
+            sum += num;
+        }
+        token = strtok(NULL, delimiter);
+    }
+
+    free(numbersCopy);
+    return sum;
+}
+
 
